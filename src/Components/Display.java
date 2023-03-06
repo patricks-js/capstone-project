9ac1a8648897b2d34961078a4eee52e310e9298c
Lucas Patrick <lucas.patrick.lsilva@gmail.com>
2023-03-06 14:12:00 -0300

    update

--- src/Components/Display.java
+++ src/Components/Display.java
@@ -1,32 +1,24 @@
 package Components;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Display {
-    private int validValue(String message, int validValue) {
-        Scanner scanner = new Scanner(System.in);
-        int value;
+    private int validValue(int value, int validValue) {
         boolean valid = false;
         do {
-            System.out.print(message);
-            value = scanner.nextInt();
             if (validValue >= 0) {
                 valid = true;
             } else {
-                System.out.println("Invalid value. Try again.");
+                System.err.println("Missing width arguments");
+                System.exit(1);
             }
         } while (!valid);
         return value;
     }
 
-    private int validValues(String message, int[] validValues) {
-        Scanner scanner = new Scanner(System.in);
-        int value;
+    private int validValues(int value, int[] validValues) {
         boolean valid = false;
         do {
-            System.out.print(message);
-            value = scanner.nextInt();
             for (int validValue : validValues) {
                 if (value == validValue) {
                     valid = true;
@@ -34,30 +26,27 @@ public class Display {
                 }
             }
             if (!valid) {
-                System.out.println("Invalid value. Try again.\n");
+                System.err.println("Missing width arguments");
+                System.exit(1);
             }
         } while (!valid);
         return value;
     }
 
-    public ArrayList<Integer> menu() {
-        ArrayList<Integer> values = new ArrayList<Integer>();
+    public ArrayList<Integer> menu(int w, int h, int g, int s) {
+        ArrayList<Integer> values = new ArrayList<>();
 
-        values.add(validValues("Enter the width of th grid [10, 20, 40, 80]: ", new int[]{10, 20, 40, 80}));
-        values.add(validValues("Enter the height of th grid [10, 20, 40]: ", new int[]{10, 20, 40}));
-        values.add(validValue("Enter the max generations (0 = infinity): ", 0));
-        values.add(validValues("Enter the fps [250, 1000]: ", new int[]{250, 1000}));
+        values.add(validValues(w, new int[]{10, 20, 40, 80}));
+        values.add(validValues(h, new int[]{10, 20, 40}));
+        values.add(validValue(g, 0));
+        values.add(validValues(s, new int[]{250, 1000}));
 
         return values;
     }
 
-    public String getFirstGeneration() {
-        Scanner scanner = new Scanner(System.in);
-        String value;
+    public String getFirstGeneration(String value) {
         boolean valid = false;
         do {
-            System.out.print("Enter the first generation (#, 0, 1): ");
-            value = scanner.nextLine();
             String validChar = "#01";
             valid = true;
             for (int i = 0; i < value.length(); i++) {
@@ -68,7 +57,8 @@ public class Display {
             }
 
             if (!valid) {
-                System.out.println("Invalid value. Try again.");
+                System.err.println("Missing width arguments");
+                System.exit(1);
             }
         } while (!valid);
         return value;
--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -3,31 +3,17 @@ import Components.Display;
 import java.util.ArrayList;
 
 public class GameOfLife {
-    /**
-     * W = 10
-     * H = 10
-     * G = 500
-     * S = 1000
-     * P = "##10101#1#01" // A # sinaliza a coluna e o numero é a cell
-     * */
     public static void main(String[] args) throws InterruptedException {
-
         int w = Integer.parseInt(args[0]);
         int h = Integer.parseInt(args[1]);
         int maxGen = Integer.parseInt(args[2]);
         int fps = Integer.parseInt(args[3]);
         String firstGen = args[4];
 
-        System.out.print(w);
-        System.out.print(h);
-        System.out.print(maxGen);
-        System.out.print(fps);
-        System.out.print(firstGen);
-
         Display newGame = new Display();
 
-        ArrayList<Integer> values = newGame.menu();
-        String firstGeneration = newGame.getFirstGeneration();
+        ArrayList<Integer> values = newGame.menu(w, h, maxGen, fps);
+        String firstGeneration = newGame.getFirstGeneration(firstGen);
 
         newGame.playGOL(values, firstGeneration);
     }
